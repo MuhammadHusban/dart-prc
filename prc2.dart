7820c6070e8ac6aad771d40fd4ffbf0ea6ea1dc5
MuhammadHusban <92823901+MuhammadHusban@users.noreply.github.com>
2022-10-05 08:59:59 -0700

    Add files via upload

---
+++ prc2.dart
@@ -0,0 +1,46 @@
+void main() {
+//   print("helo");
+// int num1 = 31000;
+// if( num1%2 == 0 && num1%3 == 0 ){ 
+//   print("right");
+
+// }
+// else if(num1% 2 == 0){
+//   print("2");
+// }
+// else if(num1%3 == 0){
+//   print("3");
+// }
+// else{
+//   print("null");
+// }
+
+// num x = 1;
+// for (int x =1;x<=10;x++){
+//   if (x == 5){
+//     break;
+//   }
+//   print(x);
+
+// }
+// while(z<=10){
+//   if(z == 5){
+//     break;
+//   }
+//   print(z);
+//   z++;
+
+// }
+
+// do{
+//   print(x);
+//   x++;
+// }while(x<=10);
+var x= getNumber(10,20);
+print(x*2);
+}
+
+getNumber(int a,int b) => (a*b)+10;
+
+
+
